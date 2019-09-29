4dec742875bcb85a9e47dcd6ea14c78d00b4833b
apple <apple@appledeiMac.local>
2019-09-29 20:20:36 +0800

    temp

--- Swift Playgrounds /14. Scope.playground/Pages/2. App Exercise - Step Competition.xcplaygroundpage/Contents.swift
+++ Swift Playgrounds /14. Scope.playground/Pages/2. App Exercise - Step Competition.xcplaygroundpage/Contents.swift
@@ -8,6 +8,18 @@
 struct User {
     var name: String
     var stepsToday: Int
+    init(name:String, stepsToday:Int) {
+        self.name = name
+        self.stepsToday = stepsToday
+    }
+    init?(name:String?, stepsToday:Int?) {
+        if let name = name, let stepsToday = stepsToday {
+            self.name = name
+            self.stepsToday = stepsToday
+        } else {
+            return nil
+        }
+    }
 }
 
 let stepMaster = User(name: "StepMaster", stepsToday: 8394)
@@ -24,8 +36,8 @@ func getWinner(competitors: [User]) -> User? {
     var topCompetitor: User?
 
     for competitor in competitors {
-        if let topCompetitor = topCompetitor {
-            if competitor.stepsToday > topCompetitor.stepsToday {
+        if let topCompetitorWithValue = topCompetitor {
+            if competitor.stepsToday > topCompetitorWithValue.stepsToday {
                 topCompetitor = competitor
             }
         } else {
@@ -34,6 +46,7 @@ func getWinner(competitors: [User]) -> User? {
     }
     return topCompetitor
 }
+print(getWinner(competitors: competitors))
 /*:
  Write a memberwise initializer inside the `User` struct above that uses variable shadowing for naming the parameters of the initializer.
  */
--- Swift Playgrounds /16. Protocols.playground/Pages/1. Exercise - Adopt Protocols.xcplaygroundpage/Contents.swift
+++ Swift Playgrounds /16. Protocols.playground/Pages/1. Exercise - Adopt Protocols.xcplaygroundpage/Contents.swift
@@ -3,27 +3,53 @@
  
  Create a `Human` class with two properties: `name` of type `String`, and `age` of type `Int`. You'll need to create a memberwise initializer for the class. Initialize two `Human` instances.
  */
+import Foundation
 
+class Human: CustomStringConvertible, Equatable, Comparable {
+    var name:String
+    var age:Int
+    var description: String {
+        return "name:\(name) age:\(age)"
+    }
+    init(name:String, age:Int) {
+        self.name = name
+        self.age = age
+    }
+    static func == (lhs:Human, rhs:Human) -> Bool {
+        return lhs.name == rhs.name && lhs.age == rhs.age
+    }
+    static func < (lhs:Human, rhs:Human) -> Bool {
+        return lhs.age < rhs.age
+    }
+}
+var human1 = Human(name: "sam", age: 20)
+var human2 = Human(name: "sara", age: 21)
 
 /*:
  Make the `Human` class adopt the `CustomStringConvertible`. Print both of your previously initialized `Human` objects.
  */
-
+print(human1)
+print(human2)
 
 /*:
  Make the `Human` class adopt the `Equatable` protocol. Two instances of `Human` should be considered equal if their names and ages are identical to one another. Print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are equal to eachother (using `==`). Then print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are not equal to eachother (using `!=`).
  */
-
+print(human1 == human2)
+print(human1 != human2)
 
 /*:
  Make the `Human` class adopt the `Comparable` protocol. Sorting should be based on age. Create another three instances of a `Human`, then create an array called `people` of type `[Human]` with all of the `Human` objects that you have initialized. Create a new array called `sortedPeople` of type `[Human]` that is the `people` array sorted by age.
  */
-
+var someone1 = Human(name: "a", age: 23)
+var someone2 = Human(name: "b", age: 21)
+var someone3 = Human(name: "c", age: 22)
+var people = [someone1, someone2, someone3]
+var sortedPeople = people.sorted()
+print(sortedPeople)
 
 /*:
  Make the `Human` class adopt the `Codable` protocol. Create a `JSONEncoder` and use it to encode as data one of the `Human` objects you have initialized. Then use that `Data` object to initialize a `String` representing the data that is stored, and print it to the console.
  */
-import Foundation
 
 
 //: page 1 of 5  |  [Next: App Exercise - Printable Workouts](@next)
