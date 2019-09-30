a14a767efe2cb23640fee3b22315962723ca133b
xyc568535797 <32026186+xyc568535797@users.noreply.github.com>
2019-09-30 10:32:54 +0800

    exercise 12 missed, now finished

--- Swift Playgrounds /12. Type Casting.playground/Pages/1. Exercise - Type Casting and Inspection.xcplaygroundpage/Contents.swift
+++ Swift Playgrounds /12. Type Casting.playground/Pages/1. Exercise - Type Casting and Inspection.xcplaygroundpage/Contents.swift
@@ -4,25 +4,67 @@
  Create a collection of type [Any], including a few doubles, integers, strings, and booleans within the collection. Print the contents of the collection.
  */
 
-
+var someCollection:[Any] = [1.0, 1, "some string", false]
+print(someCollection)
 /*:
  Loop through the collection. For each integer, print "The integer has a value of ", followed by the integer value. Repeat the steps for doubles, strings and booleans.
  */
-
+for item in someCollection {
+	switch item {
+    case is Int:
+			print("The integer has a value of \(item)")
+		
+    case is Double:
+			print("The Double has a value of \(item)")
+		
+    case is String:
+			print("The String has a value of \(item)")
+		
+    case is Bool:
+			print("The Bool has a value of \(item)")
+    default:
+			print("unknown type")
+	}
+}
 
 /*:
  Create a [String : Any] dictionary, where the values are a mixture of doubles, integers, strings, and booleans. Print the key/value pairs within the collection
  */
 
-
+var someDic:[String:Any] = ["Some Double":1.0, "Some Int":1, "Some String":"some string", "Some Bool":true]
+print(someDic)
 /*:
  Create a variable `total` of type `Double` set to 0. Then loop through the dictionary, and add the value of each integer and double to your variable's value. For each string value, add 1 to the total. For each boolean, add 2 to the total if the boolean is `true`, or subtract 3 if it's `false`. Print the value of `total`.
  */
-
+var total:Double = 0
+for (key, value) in someDic {
+	switch value {
+    case is Int, is Double:
+			total += value as! Double
+		
+    case is String:
+			total += 1
+		
+    case is Bool:
+			total += value as! Bool ? 2 : -3
+    default:
+        break
+	}
+}
+print(total)
 
 /*:
  Create a variable `total2` of type `Double` set to 0. Loop through the collection again, adding up all the integers and doubles. For each string that you come across during the loop, attempt to convert the string into a number, and add that value to the total. Ignore booleans. Print the total.
  */
-
+var total2:Double = 0
+for item in someCollection {
+	if item is Int || item is Double {
+		total2 += item as! Double
+    }
+	if item is String, let value = item as? Double {
+		total2 += value
+	}
+}
+print(total2)
 
 //: page 1 of 2  |  [Next: App Exercise - Workout Types](@next)
--- Swift Playgrounds /12. Type Casting.playground/Pages/2. App Exercise - Workout Types.xcplaygroundpage/Contents.swift
+++ Swift Playgrounds /12. Type Casting.playground/Pages/2. App Exercise - Workout Types.xcplaygroundpage/Contents.swift
@@ -46,12 +46,30 @@ var workouts: [Workout] = [
  Write simple functions called `describeRun(runningWorkout:)` and `describeSwim(swimmingWorkout:)` that take a `Run` object and a `Swim` object, respectively. Neither should return values. Each function should print a description of the workout, including the run's cadence or the swim's stroke. Time is represented in seconds, distance is represented in meters, and cadence is represented in steps per minute.
  */
 
+func describeRun(runningWorkout:Run) {
+	print(runningWorkout)
+}
 
+func describeSwim(swimmingWorkout:Swim) {
+	print(swimmingWorkout)
+}
 /*:
  Now loop through each workout in `workouts` and, using type casting, call either `describeRun(runningWorkout:)` or `describeSwim(swimmingWorkout:)` on each. Observe what is printed to the console.
  */
-
-
+for workout in workouts {
+	if let swim = workout as? Swim {
+		describeSwim(swimmingWorkout:swim)
+	}
+	if let run = workout as? Run {
+		describeRun(runningWorkout:run)
+	}
+}
+// func describeWorkout(_ workout:Workout) {
+//	print(workout)
+//}
+//for workout in workouts {
+//	describeWorkout(workout)
+//}
 /*:
 
  _Copyright © 2018 Apple Inc._
